9bd8b7f6fdf6bdb3d7ce5235820a24ff1fa13921
wonjae <dnjswo6253@naver.com>
2022-05-11 23:10:10 +0900

    게시글 수정 삭제 버그 해결

--- community/serializers.py
+++ community/serializers.py
@@ -8,15 +8,15 @@ class PostCreateSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(use_url=True, required = False)
     # category = serializers.PrimaryKeyRelatedField(queryset=query)
     # tags = serializers.PrimaryKeyRelatedField(queryset=query, many=True)
-    
+
     class Meta:
         model = Post
         fields = ['title', 'content','user','category', 'tags', 'image']
-    
+
 class PostListSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source = 'user.nick_name')
     category = serializers.ReadOnlyField(source = 'category.name')
-    
+
     class Meta:
         model = Post 
         fields = ['id', 'user', 'title', 'category','content', 'create_dt', 'like', 'view_count']
@@ -24,7 +24,7 @@ class PostListSerializer(serializers.ModelSerializer):
 
 class CommentSerializerSub(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
-    
+
     class Meta:
         model = Comment
         fields = ['id', 'author', 'content', 'update_dt']
@@ -45,13 +45,13 @@ class PostSerializerSub(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title']
-            
+
 class PostDetailSerializer(serializers.Serializer):
     post = PostRetrieveSerializer()
     prevPost = PostSerializerSub()
     nextPost = PostSerializerSub()
     commentList = CommentSerializerSub(many = True)
-    
+
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source = 'user.nick_name')
 
@@ -70,7 +70,7 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['name']
 
 class TagSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Tag
         fields = ['name']
@@ -80,10 +80,9 @@ class CateTagSerializer(serializers.Serializer):
     tagList = serializers.ListField(child=serializers.CharField())
 
 class PostModifySerializer(serializers.ModelSerializer):
-    content = serializers.StringRelatedField()
-    title = serializers.StringRelatedField()
-    id = serializers.StringRelatedField()
-    
+    content = serializers.CharField()
+    title = serializers.CharField()
+
     class Meta:
         model = Post
-        fields = ['content', 'title', 'id']
+        fields = ['content', 'title']
